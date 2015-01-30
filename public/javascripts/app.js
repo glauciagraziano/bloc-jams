eee30aa8bd13197fdca339ef3f5f09c70da8fa62
glauciagraziano <graziano.glaucia@gmail.com>
2015-01-30 03:49:39 +0000

    Converted collection view to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -297,6 +297,24 @@ var albumPicasso = {
  //require('./album');
  //require('./collection');
  //require('./profile');
+
+// Example album.
+ var albumPicasso = {
+   name: 'The Colors',
+   artist: 'Pablo Picasso',
+   label: 'Cubism',
+   year: '1881',
+   albumArtUrl: '/images/album-placeholder.png',
+ 
+   songs: [
+       { name: 'Blue', length: '4:26' },
+       { name: 'Green', length: '3:14' },
+       { name: 'Red', length: '5:01' },
+       { name: 'Pink', length: '3:21'},
+       { name: 'Magenta', length: '2:15'}
+     ]
+ };
+ 
  
  blocJams = angular.module('BlocJams', ['ui.router']);
  blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
@@ -306,6 +324,16 @@ var albumPicasso = {
      url: '/',
      controller: 'Landing.controller',
      templateUrl: '/templates/landing.html'
+   });
+   $stateProvider.state('collection', {
+     url: '/collection',
+     controller: 'Collection.controller',
+     templateUrl: '/templates/collection.html'
+   });
+   $stateProvider.state('Allbum', {
+     url: '/album',
+     controller: 'Album.controller',
+     templateUrl: '/templates/album.html'
    });
  }]);
  
@@ -340,6 +368,13 @@ var albumPicasso = {
      
    };  
  }]);
+ 
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+  $scope.albums = [];
+   for (var i = 0; i < 33; i++) {
+     $scope.albums.push(angular.copy(albumPicasso));
+   }
+}]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
