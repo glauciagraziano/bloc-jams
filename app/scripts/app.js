a19cc24b595cb66e33c0146305b02d854b5ba92a
glauciagraziano <graziano.glaucia@gmail.com>
2015-02-17 17:36:39 +0000

    Added volume control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -137,6 +137,14 @@
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
    
+   $scope.volumeClass = function volumeClass() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+   
    SongPlayer.onTimeUpdate(function(event, time) {
      $scope.$apply(function(playTime){
        $scope.playTime = time;
@@ -155,6 +163,7 @@
     currentSong: null,
     currentAlbum: null,
     playing: false,
+    volume: 90,
     
     play: function() {
       this.playing = true;
@@ -198,17 +207,26 @@
     onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
+    
+    setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
+    
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
       }
       this.currentAlbum = album;
       this.currentSong = song;
-      
       currentSoundFile = new buzz.sound(song.audioUrl, {
         formats: [ "mp3" ],
         preload: true
       });
+      
+      currentSoundFile.setVolume(this.volume);
       
       currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -432,6 +432,14 @@ var albumPicasso = {
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
    
+   $scope.volumeClass = function volumeClass() {
+     return {
+       'fa-volume-off': SongPlayer.volume == 0,
+       'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+       'fa-volume-up': SongPlayer.volume > 70
+     }
+   }
+   
    SongPlayer.onTimeUpdate(function(event, time) {
      $scope.$apply(function(playTime){
        $scope.playTime = time;
@@ -450,6 +458,7 @@ var albumPicasso = {
     currentSong: null,
     currentAlbum: null,
     playing: false,
+    volume: 90,
     
     play: function() {
       this.playing = true;
@@ -493,17 +502,26 @@ var albumPicasso = {
     onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
+    
+    setVolume: function(volume) {
+      if(currentSoundFile){
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
+    
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
       }
       this.currentAlbum = album;
       this.currentSong = song;
-      
       currentSoundFile = new buzz.sound(song.audioUrl, {
         formats: [ "mp3" ],
         preload: true
       });
+      
+      currentSoundFile.setVolume(this.volume);
       
       currentSoundFile.bind('timeupdate', function(e){
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
